697fbcdccfce5829cd475edea709bbaabbcabf56
bxsh <rizzbash32@gmail.com>
2026-06-22 13:27:36 -0400

    Fix typo in loadstring function for game ID

--- loader.lua
+++ loader.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 13822889 then
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/iHavebraincells/velra-lt2/main/main.lua"))()
 		elseif game.GameId == 155615604 then
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/iHavebraincells/velra-prisonlife/refs/heads/main/main.lua"))()
+		elseif game.GameId == 97598239454123 then
+			loadsting(game:HttpGet("https://raw.githubusercontent.com/iHavebraincells/velra-gag2/refs/heads/main/main.lua"))()
 		end
